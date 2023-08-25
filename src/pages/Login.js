2b5a69518474f5e628660b0f19895ba34c2fd376
pablo4lzate <juanpablo.alzate.hincapie@gmail.com>
2023-08-25 13:36:55 -0700

    Changed CSS. Not definitive

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Login = () => {
     return (
-        <div>
+        <div className="loginPage">
             <h1>Login Page</h1>
-            {/* Other content */}
+            <p>Sign In:</p>
+            <button className="login-button">
+                Sign in
+            </button>
+            <p>Register: </p>
+            <button className="register-button">
+                Register
+            </button>
         </div>
     );
 }
